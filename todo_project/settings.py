ea042c7871faae052cd25fc334b203db01f7337e
Hiyashinsuuu <keisshalouise.canios@1.ustp.edu.ph>
2025-03-18 01:01:11 +0800

    s

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/django_static/'
+STATIC_ROOT = '/home/ubuntu/todo_project/todo_project/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
