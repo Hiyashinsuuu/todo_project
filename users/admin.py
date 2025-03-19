ca6cad5df65abf6418fa67462499c0014e2f1b90
Hiyashinsuuu <keisshalouise.canios@1.ustp.edu.ph>
2025-03-19 17:37:16 +0800

    asdfas

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from .models import CustomUser
 
-
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'get_full_name', 'is_active')
+    list_display = ('username', 'email', 'get_full_name', 'is_active')  
 
     def get_full_name(self, obj):
         return f"{obj.first_name} {obj.last_name}".strip()
     get_full_name.short_description = 'Full Name'
-
 
 admin.site.register(CustomUser, CustomUserAdmin)
