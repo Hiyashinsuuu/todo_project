26855e6d90bfddd8a89abca8c26d7b33d6aa5c38
Hiyashinsuuu <keisshalouise.canios@1.ustp.edu.ph>
2025-03-20 11:06:00 +0800

    huhu

--- todos/admin.py
+++ todos/admin.py
@@ -5,8 +5,7 @@ from .models import Task
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'is_important', 'is_completed', 'user')  
     list_display_links = ('title',)  # Title is clickable to edit full task details
-    list_editable = ('description','is_important', 'is_completed')  # These fields are editable in list view
-    list_filter = ('is_completed')  # Add filters
+    list_editable = ('description','is_important', 'is_completed') 
     search_fields = ('title', 'user__username')  # Search bar for tasks and usernames
 
 
