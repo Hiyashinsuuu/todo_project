655616dc74ffe2e9eb91e87db2c761b35df25836
Hiyashinsuuu <keisshalouise.canios@1.ustp.edu.ph>
2025-03-20 10:32:56 +0800

    huhu

--- todos/models.py
+++ todos/models.py
@@ -14,7 +14,6 @@ class Project(models.Model):
     
     id = models.PositiveSmallIntegerField(primary_key=True)  # ✅ Remove choices
     name = models.CharField(max_length=255, unique=True, null=True, blank=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.DEFAULT_CHOICES.get(self.id, "Unknown")  # ✅ Prevent key errors
