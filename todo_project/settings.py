51643492b0913ab710566ab93ee3780d4984ccde
Hiyashinsuuu <keisshalouise.canios@1.ustp.edu.ph>
2025-03-21 19:36:44 +0800

    huhu

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -215,9 +215,8 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",  # Vite frontend URL
-    "https://mongrel-singular-legally.ngrok-free.app", 
-    "http:127.0.0.1:8000",
-    "https://alisto-main-d4xv.vercel.app/",
+    # "https://mongrel-singular-legally.ngrok-free.app", 
+    "https://alisto-main-d4xv.vercel.app/"
 ]
 
 CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
