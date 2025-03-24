7b5967e983bc003d7ece7aa34019eb8b55180459
Hiyashinsuuu <keisshalouise.canios@1.ustp.edu.ph>
2025-03-24 12:07:54 +0800

    pass

--- users/urls.py
+++ users/urls.py
@@ -4,13 +4,13 @@ from .views import *
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('verify-email/<uidb64>/<token>/', VerifyEmailView.as_view(), name='email-verify'),
+    path('verify-email/<token>/', VerifyEmailView.as_view(), name='email-verify'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('user/', get_user_details, name='get_user_details'),
     path('update-password/', update_user_password, name='update_user_password'),
     path('update/', update_user_details, name='update_user_details'),
     path("google/", GoogleLoginView.as_view(), name="google_login"),
     path('user/upload-profile-picture/', UploadProfilePictureView.as_view(), name='upload-profile-picture'),
-    path('password-reset/<uidb64>/<token>/', PasswordResetView.as_view(), name='password-reset'),
-    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+    path('password-reset/', PasswordResetView.as_view(), name='password-reset'),
+    path('password-reset-confirm/<token>/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
 ]
